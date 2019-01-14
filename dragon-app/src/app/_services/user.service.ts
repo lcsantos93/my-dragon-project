fa360ecf0190afb9853a984a4e2540972ce1af40
Lucas <s.cardosolucas@gmail.com>
2019-01-13 22:55:34 -0200

    Update globals variables for environments.

--- dragon-app/src/app/_services/user.service.ts
+++ dragon-app/src/app/_services/user.service.ts
@@ -2,28 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { User } from '@/_models/user';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   constructor(private http: HttpClient) { }
-
+  private API_URL= environment.API_URL;
   getAll() {
-    return this.http.get<User[]>(`${config.apiUrl}/users`);
+    return this.http.get<User[]>(`${this.API_URL}/users`);
   }
 
   getById(id: number) {
-    return this.http.get(`${config.apiUrl}/users/${id}`);
+    return this.http.get(`${this.API_URL}/users/${id}`);
   }
 
   register(user: User) {
-    return this.http.post(`${config.apiUrl}/users/register`, user);
+    return this.http.post(`${this.API_URL}/users/register`, user);
   }
 
   update(user: User) {
-    return this.http.put(`${config.apiUrl}/users/${user.id}`, user);
+    return this.http.put(`${this.API_URL}/users/${user.id}`, user);
   }
 
   delete(id: number) {
-    return this.http.delete(`${config.apiUrl}/users/${id}`);
+    return this.http.delete(`${this.API_URL}/users/${id}`);
   }
 }
